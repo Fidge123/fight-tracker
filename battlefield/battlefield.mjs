2433f648439efeaa587d04cbcebc15f9b26e225e
Florian Richter <florian@lkr24.de>
2019-10-10 15:20:07 +0200

    Move scripts to html file where applicable

--- battlefield/battlefield.mjs
+++
@@ -1,7 +0,0 @@
-import { Counter } from './counter.mjs';
-import { drawListOfFighters } from '../fighters/fighters.mjs';
-
-const counter = new Counter('counter', 'next-round', 'reset');
-counter.startCounter();
-
-drawListOfFighters('fighters');
--- fighters/index.mjs
+++
@@ -1,7 +0,0 @@
-import { createFighter, drawListOfFighters } from './fighters.mjs';
-
-drawListOfFighters('fighters');
-document.getElementById('create').onclick = () => {
-  createFighter({ name: document.getElementById('fname').value, ini: document.getElementById('fini').value });
-  drawListOfFighters('fighters');
-};
